80a52af51556bdc80d0ba6a89f21851b86816384
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-01-31 19:29:53 +0100

    UserExperience Entity

---
+++ Domain/UserExperience.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain
+{
+    public class UserExperience
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string CompanyName { get; set; }
+        public string Role { get; set; }
+        public string Description { get; set; }
+    }
+}
