153b87d49e6d0bb74f623f3b1a4f72dff061f899
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-01-31 19:27:52 +0100

    UserEntity Added

---
+++ Domain/ApplicationUser .cs
@@ -0,0 +1,15 @@
+﻿namespace Domain
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? ContactEmail { get; set; }
+        public int? PhoneNumber { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public string? PhotoUrl { get; set; }
+        public string? About { get; set; }
+    }
+}
