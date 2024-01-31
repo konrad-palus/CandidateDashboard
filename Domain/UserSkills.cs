f168e864abc6b79d95196f6e5972e9b675393157
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-01-31 19:36:32 +0100

    UserSkills entity added

---
+++ Domain/UserSkills.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain
+{
+    public class UserSkills
+    {
+        public int Id { get; set; }
+        public string SkillName { get; set; }
+        public string Description { get; set; }
+    }
+}
