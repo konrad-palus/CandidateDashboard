e4cde82db15ed651683c8494eb4f59ff9b6b633d
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-01-31 19:28:57 +0100

    UserEducation Entity

---
+++ Domain/UserEducation.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain
+{
+    public class UserEducation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
