0454f6187ebb680324c737f73afc8f0b2d49dba6
konrad-palus <117956112+konrad-palus@users.noreply.github.com>
2024-02-02 15:57:29 +0100

    IAccountControler added - abstraction layer

---
+++ CandidateDashboardApi/Interfaces/IAccountController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CandidateDashboardApi.Interfaces
+{
+    public interface IAccountController
+    {
+        public Task<IActionResult> RegisterUserAsync(string login, string registrationEmail, string password, bool isCandidate, string? name, string? lastName);
+        public Task<IActionResult> LoginUserAsync(string login, string password);
+
+    }
+}
